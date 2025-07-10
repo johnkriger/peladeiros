e42aae44502360538897d865a239d2f7c80b95bb
John Kryger <john.kryger.webart@gmail.com>
2025-07-10 16:44:07 -0300

    Iniciando projeto.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <title>Peladeiros</title>
+</head>
+<body>
+    <div>Iniciando Projeto!</div>
+</body>
+</html>
